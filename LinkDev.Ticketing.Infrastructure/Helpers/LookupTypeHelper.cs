ba499041b28fb71a8bbd2c68a5f06a59bd44c730
Ahmed.Gaber <Ahmed.Gaber@DESKTOP-MDES0HQ>
2026-03-11 22:53:55 +0200

    fix attachments

--- LinkDev.Ticketing.Infrastructure/Helpers/LookupTypeHelper.cs
+++ LinkDev.Ticketing.Infrastructure/Helpers/LookupTypeHelper.cs
@@ -1,32 +1,12 @@
 ﻿using LinkDev.Ticketing.Domain.Entities;
 using LinkDev.Ticketing.Domain.Enums;
 using LinkDev.Ticketing.Infrastructure.Data;
-using Microsoft.EntityFrameworkCore;
 
 namespace LinkDev.Ticketing.Infrastructure.Helpers
 {
     public class LookupTypeHelper
     {
-        //public static DbSet<TEntity> GetLookupSource<TEntity>(LookupType lookupType, TicketingContext ticketingContext) where TEntity : BaseLookup
-        //{
-        //    switch (lookupType)
-        //    {
-        //        case LookupType.TicketType:
-        //            return (DbSet<TEntity>)(object)ticketingContext.Set<TicketTypeLookup>();
-        //        case LookupType.TicketCategory:
-        //            return (DbSet<TEntity>)(object)ticketingContext.Set<TicketCategoryLookup>();
-        //        case LookupType.TicketPriority:
-        //            return (DbSet<TEntity>)(object)ticketingContext.Set<TicketPriorityLookup>();
-        //        case LookupType.TicketStatus:
-        //            return (DbSet<TEntity>)(object)ticketingContext.Set<TicketStatusLookup>();
-        //        default:
-        //            throw new ArgumentException("Invalid lookup type");
-        //    }
-        //}
-
-        public static IQueryable<BaseLookup> GetLookupSource(
-        LookupType lookupType,
-        TicketingContext ticketingContext)
+        public static IQueryable<BaseLookup> GetLookupSource(LookupType lookupType, TicketingContext ticketingContext)
         {
             switch (lookupType)
             {
